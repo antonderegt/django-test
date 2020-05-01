b66448d1b4121fe24dbed84234e0745779947db4
Anton <antonderegt@pm.me>
2020-05-01 12:00:07 +0200

    Changes admin page header

--- polls/models.py
+++ polls/models.py
@@ -6,11 +6,17 @@ from django.utils import timezone
 class Question(models.Model):
 	question_text = models.CharField(max_length=200)
 	pub_date = models.DateTimeField('date published')
+
 	def __str__(self):
 		return self.question_text
+
 	def was_published_recently(self):
-            now = timezone.now()
-            return now - datetime.timedelta(days=1) <= self.pub_date <= now
+		now = timezone.now()
+		return now - datetime.timedelta(days=1) <= self.pub_date <= now
+
+Question.was_published_recently.admin_order_field = 'pub_date'
+Question.was_published_recently.boolean = True
+Question.was_published_recently.short_description = 'Published recently?'
 
 class Choice(models.Model):
 	question = models.ForeignKey(Question, on_delete=models.CASCADE)
